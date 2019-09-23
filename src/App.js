f1dfbac00ee86bcf991e927d067c819b0ab3924a
Vijay <vijaybalu1989@gmail.com>
2019-09-23 17:49:18 +0530

    feat: using query params

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import queryString from 'query-string'
 
+const queryParam = ({location}) => {
+  return (
+    <div>
+      {queryString.parse(location)}
+    </div>
+  )
+}
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Course
-      </div>
+      <Router>
+        <Route path={'/'} component={queryParam} />
+      </Router>
     )
   }
 }
