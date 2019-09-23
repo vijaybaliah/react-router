5f7102a2bde341ebbe35de9858f56bbfe386e061
Vijay <vijaybalu1989@gmail.com>
2019-09-23 15:08:27 +0530

    feat: added url parameters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
+const Child = ({ match }) => {
+  <div>
+    <h3>ID: {match.params.id}</h3>
+  </div>
+}
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Course
-      </div>
+      <Router>
+        <h2>Accounts</h2>
+        <ul>
+          <li><Link to={'/netflix'}>Netflix</Link></li>
+          <li><Link to={'/zillow-group'}>Zillow Group</Link></li>
+          <li><Link to={'/yahoo'}>Yahoo</Link></li>
+          <li><Link to={'/modus-create'}>Modus Create</Link></li>
+        </ul>
+        <Route path={'/:id'} component={Child}/>
+      </Router>
     )
   }
 }
