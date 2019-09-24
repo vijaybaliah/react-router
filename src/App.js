7510d711c97da60527f60bc0c0279327081e5bfb
Vijay <vijaybalu1989@gmail.com>
2019-09-24 17:32:31 +0530

    feat: added custom links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const OldSchoolMenuLink = ({children, exact, to}) => (
+  <Route exact={exact} path={to} children={({match}) => (
+    <div>
+      {match ? '>': ''}
+      <Link to={to}>
+        {children}
+      </Link>
+    </div>
+  )} />
+)
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Course
-      </div>
+      <Router>
+        <OldSchoolMenuLink exact={true} to={'/'}>
+          Home
+        </OldSchoolMenuLink>
+        <OldSchoolMenuLink to={'/about'}>
+          About
+        </OldSchoolMenuLink>
+        <Route exact path={'/'} component={Home} />
+        <Route path="/about" component={About}/>
+      </Router>
     )
   }
 }
