63bc190e56a1eec97716be735659656f17de91a0
Vijay <vijaybalu1989@gmail.com>
2019-09-24 18:50:05 +0530

    feat: setting up home routes

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './Navbar'
+import Home from './Home'
+import Players from './Players'
+import Teams from './Teams'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/players' component={Players} />
+            <Route path='/teams' component={Teams} />
+            <Route render={() => <h1 className="text-center">Four oh Four.</h1>}/>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
