947ea848ca800b7096a1b9d888ec32dfe8310e64
Vijay <vijaybalu1989@gmail.com>
2019-09-24 13:47:41 +0530

    feat: ambiguous routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+const About = () => <h2>About</h2>
+const Company = () => <h2>Company</h2>
+const User = ({ match }) => (
+  <div>
+    <h2>User: {match.params.user}</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Course
-      </div>
+      <Router>
+        <ul>
+          <li><Link to='/about'>About Us (static)</Link></li>
+          <li><Link to='/company'>Company (static)</Link></li>
+          <li><Link to='/kim'>Kim (dynamic)</Link></li>
+          <li><Link to="/chris">Chris (dynamic)</Link></li>
+        </ul>
+        <Switch>
+          <Route path='/about'  component={About} />
+          <Route path='/company'  component={Company} />
+          <Route path='/:user'  component={User} />
+        </Switch>
+      </Router>
     )
   }
 }
