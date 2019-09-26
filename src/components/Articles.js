30b5a9ae78e069f20186c38549ef97090db29db3
Vijay <vijaybalu1989@gmail.com>
2019-09-26 15:21:19 +0530

    feat: added loading

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import { getTeamArticles } from '../api'
 import Article from './Article'
+import Loading from './Loading'
 
 export default class Articles extends Component {
   state = {
@@ -25,7 +26,7 @@ export default class Articles extends Component {
     const { params, url } = this.props.match
     const { teamId } = params
     return loading === true
-    ? <h1>Loading</h1>
+    ? <Loading />
     : <div className='container two-column'>
         <Sidebar
           loading={loading}
@@ -35,7 +36,7 @@ export default class Articles extends Component {
         />
         <Route path={`${url}/:articleId`} render={({ match }) => (
           <Article articleId={match.params.articleId} teamId={teamId}>
-            {(article) => !article ? <h1>Loading</h1> : (
+            {(article) => !article ? <Loading /> : (
               <div className='panel'>
                 <article className='article' key={article.id}>
                   <h1 className='header'>{article.title}</h1>
