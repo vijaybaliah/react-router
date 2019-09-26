37e9092e3c64e9d343b071a397cc7abe2b1619ed
Vijay <vijaybalu1989@gmail.com>
2019-09-26 15:01:32 +0530

    feat: added articles

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Home from './Home'
 import Players from './Players'
 import Teams from './Teams'
 import TeamPage from './TeamPage'
+import Articles from './Articles'
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route path='/players' component={Players} />
             <Route path='/teams' component={Teams} />
             <Route path=':/teamId' component={TeamPage} />
+            <Route path='/:teamId/articles' component={Articles} />
             <Route render={() => <h1 className="text-center">Four oh Four.</h1>}/>
           </Switch>
         </div>
---
+++ src/components/Articles.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import { getTeamArticles } from '../api'
+import Article from './Article'
+
+export default class Articles extends Component {
+  state = {
+    loading: true,
+    teamsArticles: []
+  }
+
+  componentDidMount() {
+    getTeamArticles(this.props.match.params.teamId)
+      .then((teamsArticles) => {
+        this.setState({
+          loading: false,
+          teamsArticles: teamsArticles.map(article => article.title)
+        })
+      })
+  }
+
+  render() {
+    const { loading, teamsArticles } = this.state
+    const { params, url } = this.props.match
+    const { teamId } = params
+    return loading === true
+    ? <h1>Loading</h1>
+    : <div className='container two-column'>
+        <Sidebar
+          loading={loading}
+          title='Articles'
+          list={teamsArticles}
+          {...this.props}
+        />
+        <Route path={`${url}/:articleId`} render={({ match }) => (
+          <Article articleId={match.params.articleId} teamId={teamId}>
+            {(article) => !article ? <h1>Loading</h1> : (
+              <div className='panel'>
+                <article className='article' key={article.id}>
+                  <h1 className='header'>{article.title}</h1>
+                  <p>{article.body}</p>
+                </article>
+              </div>
+            )}
+          </Article>
+        )}/>
+      </div>
+  }
+} 
