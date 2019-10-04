c7a6b60464bb423baddb476b6504dab1d57c1beb
Vijay <vijaybalu1989@gmail.com>
2019-10-04 14:07:43 +0530

    feat: recursive paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const users = [
+  { id: 0, name: 'Michelle', friends: [ 1, 2, 3 ] },
+  { id: 1, name: 'Sean', friends: [ 0, 3 ] },
+  { id: 2, name: 'Kim', friends: [ 0, 1, 3 ], },
+  { id: 3, name: 'David', friends: [ 1, 2 ] }
+]
+
+const find = (id) => users.find(user => user.id === id)
+
+const Person = ({ match }) => {
+  const person = find(parseInt(match.params.id, 10))
+  return (
+    <div>
+      <h3>{person.name + '\'s'} friends</h3>
+      <ul>
+        {
+          person.friends.map((id) => (
+            <li key={`${match.url}/${id}`}><Link to={`${match.url}/${id}`}>{find(id).name}</Link></li>
+          ))
+        }
+      </ul>
+      <Route path={`${match.url}/:id`} component={Person}/>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Course
-      </div>
+      <Router>
+        <Person match={{params: { id: 0 }, url: ''}} />
+      </Router>
     )
   }
 }
